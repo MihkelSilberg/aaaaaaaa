c0ff2562144d4dcdd08a9be7be4ccbc0ba80d5d8
sfedseF <mihkel.silberg@gmail.com>
2024-04-25 09:24:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace aaaaaaaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("sitaridav2");
+        }
+    }
+}
